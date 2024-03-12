57fadbf5aaac41af3048d120c667222d81f23582
Rohith-Nagappa <rohithankith2017@gmail.com>
2024-03-12 21:56:45 +0530

    trails to add new code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MainContainer from "./Components/MainContainer";
+import Trail from "./Components/trail";
 
 const App = () => {
   return (
     <div className="App">
       <MainContainer />
+      <Trail/>
     </div>
   );
 };
---
+++ client/src/Components/trail.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Trail = () => {
+    return <h1>its me</h1>
+}
+
+export default Trail;
