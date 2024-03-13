76762abaf12a1ea13f22e3d27626e3a1d7ae9644
Sush25N <sushgabhane123@gmail.com>
2024-03-14 01:27:16 +0530

    added icons

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./main-container.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Sidebar = () => {
-  return <div className="sidebar-container">Sidebar</div>;
+  return (
+    <div className="sidebar-container">
+      <div className="sb-header">
+        <AccountCircleIcon />
+      </div>
+      <div className="sb-search">Search</div>
+      <div className="sb-conversations">Conversations</div>
+    </div>
+  );
 };
 
 export default Sidebar;
