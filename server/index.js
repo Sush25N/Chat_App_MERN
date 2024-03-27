f71d493d5da73617f7f4604206965c02c638a3f2
Sush25N <sushgabhane123@gmail.com>
2024-03-28 00:07:54 +0530

    added api for sign in(name, email,password)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
+const userRoutes = require("./Routes/userRoutes");
 
 const app = express();
 dotenv.config();
 
 // console.log(mongoose.connect(process.env.MONGO_URI));
+
+app.use(express.json());
 
 const connectDB = async () => {
   try {
@@ -21,6 +24,8 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/user", userRoutes);
 
 // console.log(process.env.MONGO_URI);
 const PORT = process.env.PORT || 5000;
