db432fb450cb363ab4f8a10740fe2bb1bc66ed1e
Sush25N <sushgabhane123@gmail.com>
2024-04-04 21:09:43 +0530

    authoized users and added fetchusers for available users

--- client/src/Components/Users.js
+++ client/src/Components/Users.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton } from "@mui/material";
 import logo from "../icons/chat.png";
 import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
+  const [refresh, setRefresh] = useState(true);
   const lightTheme = useSelector((state) => state.themeKey);
+  const [users, setUsers] = useState([]);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  const navigate = useNavigate();
+  if (!userData) {
+    console.log("User Not Authenticated");
+    navigate(-1);
+  }
+
+  useEffect(() => {
+    console.log("User refreshed");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userData.data.token}`,
+      },
+    };
+    axios.get("http://localhost:8080/user/fetchUsers", config).then((data) => {
+      console.log("User Data from API ", data);
+      setUsers(data.data);
+    });
+  }, [refresh]);
   return (
     <AnimatePresence>
       <motion.div
@@ -18,14 +43,22 @@ const Users = () => {
       >
         <div className={"ug-header" + (lightTheme ? "" : " dark")}>
           <div>
-            <IconButton>
-              <img
-                src={logo}
-                style={{ height: "2rem", width: "2rem", marginLeft: "10px" }}
-              />
-              <p className={"ug-title" + (lightTheme ? "" : " dark")}>
-                Online Users
-              </p>
+            {/* <IconButton> */}
+            <img
+              src={logo}
+              style={{ height: "2rem", width: "2rem", marginLeft: "10px" }}
+            />
+            <p className={"ug-title" + (lightTheme ? "" : " dark")}>
+              Available Users
+            </p>
+            {/* </IconButton> */}
+            <IconButton
+              className={"icon" + (lightTheme ? "" : " dark")}
+              onClick={() => {
+                setRefresh(!refresh);
+              }}
+            >
+              <RefreshIcon />
             </IconButton>
           </div>
         </div>
@@ -39,46 +72,36 @@ const Users = () => {
           />
         </div>
         <div className="ug-list">
-          <motion.div
-            whileHover={{ scale: 1.01 }}
-            whileTap={{ scale: 0.98 }}
-            className={"list-item" + (lightTheme ? "" : " dark")}
-          >
-            <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
-            <p className={"con-title" + (lightTheme ? "" : " dark")}>
-              Test Users{" "}
-            </p>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.01 }}
-            whileTap={{ scale: 0.98 }}
-            className={"list-item" + (lightTheme ? "" : " dark")}
-          >
-            <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
-            <p className={"con-title" + (lightTheme ? "" : " dark")}>
-              Test Users{" "}
-            </p>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.01 }}
-            whileTap={{ scale: 0.98 }}
-            className={"list-item" + (lightTheme ? "" : " dark")}
-          >
-            <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
-            <p className={"con-title" + (lightTheme ? "" : " dark")}>
-              Test Users{" "}
-            </p>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.01 }}
-            whileTap={{ scale: 0.98 }}
-            className={"list-item" + (lightTheme ? "" : " dark")}
-          >
-            <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
-            <p className={"con-title" + (lightTheme ? "" : " dark")}>
-              Test Users{" "}
-            </p>
-          </motion.div>
+          {users.map((user, index) => {
+            return (
+              <motion.div
+                whileHover={{ scale: 1.01 }}
+                whileTap={{ scale: 0.98 }}
+                className={"list-item" + (lightTheme ? "" : " dark")}
+                key={index}
+                onClick={() => {
+                  console.log("Creating chat with ", user.name);
+                  const config = {
+                    headers: {
+                      Authorization: `Bearer ${userData.data.token}`,
+                    },
+                  };
+                  axios.post(
+                    "https://localhost:8080/chat/",
+                    {
+                      userId: user._id,
+                    },
+                    config
+                  );
+                }}
+              >
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>
+                  {user.name}
+                </p>
+              </motion.div>
+            );
+          })}
         </div>
       </motion.div>
     </AnimatePresence>
--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -59,4 +59,23 @@ const registerController = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { loginController, registerController };
+const fetchAllUsersController = expressAsyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const users = await UserModel.find(keyword).find({
+    _id: { $ne: req.user._id },
+  });
+  res.send(users);
+});
+
+module.exports = {
+  loginController,
+  registerController,
+  fetchAllUsersController,
+};
